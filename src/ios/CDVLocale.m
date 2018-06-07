31d0dec1499f6ffd75567be7df7843ae650832b1
Pedro Jorquera <pjorquera@okode.com>
2018-06-07 08:28:14 +0200

    Added support for override country code from preferred language in iOS

--- src/ios/CDVLocale.m
+++ src/ios/CDVLocale.m
@@ -6,6 +6,13 @@
 {
     NSLocale* currentLocale = [NSLocale currentLocale];
     NSString* countryCode = [currentLocale objectForKey: NSLocaleCountryCode];
+    NSString* preferred = [[NSLocale preferredLanguages] objectAtIndex:0];
+    if (preferred != nil) {
+        NSArray* items = [preferred componentsSeparatedByString:@"-"];
+        if (items != nil && [items count] > 1) {
+            countryCode = [items objectAtIndex:1];
+        }
+    }
     CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:countryCode];
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
